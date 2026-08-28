cc15f06f8c79b3c9ffce6b47e543b6b5bf5e3cb5
Dasha <koshka_1994@bk.ru>
2026-08-28 14:56:20 +0300

    p

--- run.sh
+++ run.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-uv run celery \
-  -A app.infrastructure.celery.app:celery_app \
-  worker \
-  --loglevel=info &
-
 exec uv run uvicorn app.main:app \
   --host 0.0.0.0 \
   --port 8000
