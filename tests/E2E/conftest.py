33a0a2d1af07ecb1beb798deed7933cd22344495
Dasha <koshka_1994@bk.ru>
2026-09-01 08:48:32 +0300

    .

--- tests/E2E/conftest.py
+++ tests/E2E/conftest.py
@@ -1,35 +1,46 @@
 import pytest
+import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
 from unittest.mock import AsyncMock
 
 from app.main import app
-from app.presentation.dependencies import get_cancel_ticket_use_case, get_create_ticket_use_case, get_get_available_seats_use_case
+from app.presentation.dependencies import (
+    get_cancel_ticket_use_case,
+    get_create_ticket_use_case,
+    get_get_available_seats_use_case,
+)
 
 
 @pytest.fixture
 def cancel_ticket_use_case():
     return AsyncMock()
 
+
 @pytest.fixture
 def create_ticket_use_case():
     return AsyncMock()
+
 
 @pytest.fixture
 def get_available_seats_use_case():
     return AsyncMock()
 
-@pytest.fixture
-async def client(cancel_ticket_use_case):
+
+@pytest_asyncio.fixture
+async def client(
+    cancel_ticket_use_case,
+    create_ticket_use_case,
+    get_available_seats_use_case,
+):
     app.dependency_overrides[get_cancel_ticket_use_case] = (
         lambda: cancel_ticket_use_case
     )
-
     app.dependency_overrides[get_create_ticket_use_case] = (
         lambda: create_ticket_use_case
     )
-
     app.dependency_overrides[get_get_available_seats_use_case] = (
-    lambda: get_available_seats_use_case)
+        lambda: get_available_seats_use_case
+    )
 
     transport = ASGITransport(app=app)
 
